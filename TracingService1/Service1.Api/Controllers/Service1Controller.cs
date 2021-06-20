48f0cf4780cdac458e033fc937da6056484a8490
Feroz Ahmmed <feroz.ahmmed@selise.ch>
2021-06-20 15:51:23 +0600

    initial commit

---
+++ TracingService1/Service1.Api/Controllers/Service1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service1.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Service1Controller : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.GetAsync("http://localhost:6002/Service2");
+            var result = await response.Result.Content.ReadAsStringAsync();
+            
+            return Ok(result);
+        }
+    }
+}
---
+++ TracingService2/Service2.Api/Controllers/Service2Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service2.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Service2Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Service2";
+        }
+    }
+}
